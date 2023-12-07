a39a49c0235c2ad75c5ee340d3d6774076d28276
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2023-12-07 18:29:47 +0330

    [feat] Add more validation for both inputs and fix their styles

--- ghazal-beauty/src/utils/validateAdminInput/validateAdminInput.jsx
+++ ghazal-beauty/src/utils/validateAdminInput/validateAdminInput.jsx
@@ -1,5 +1,11 @@
 import * as Yup from "yup";
 export const validationSchema = Yup.object().shape({
-  userName: Yup.string().required("نام کاربری نمی‌تواند خالی باشد"),
-  password: Yup.string().required("رمز عبور نمی‌تواند خالی باشد"),
+  userName: Yup.string()
+    .min(3, "نام کاربری باید حداقل 3 حرف باشد")
+    .max(20, "نام کاربری نمی‌تواند بیش از 20 حرف باشد")
+    .required("نام کاربری نمی‌تواند خالی باشد"),
+  password: Yup.string()
+    .min(8, "رمز عبور باید حداقل 8 کاراکتر باشد")
+    .max(12, "رمز عبور نمی‌تواند بیش از 12 کاراکتر باشد")
+    .required("رمز عبور نمی‌تواند خالی باشد"),
 });
