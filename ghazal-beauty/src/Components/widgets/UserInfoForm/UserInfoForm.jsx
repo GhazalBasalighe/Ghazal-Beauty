dbfd99d2e539bfb83bfb838d887cbdec6ff6e173
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2023-12-28 14:38:07 +0330

    [feat] Add yup & formik validation

--- ghazal-beauty/src/Components/widgets/UserInfoForm/UserInfoForm.jsx
+++ ghazal-beauty/src/Components/widgets/UserInfoForm/UserInfoForm.jsx
@@ -1,101 +1,174 @@
+import { useState } from "react";
 import { Button } from "../../base";
+import { Eye, EyeSlash } from "@phosphor-icons/react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { userInfoFormSchema } from "../../../utils";
 
 export function UserInfoForm() {
+  const [isPasswordHidden, setIsPasswordHidden] = useState(true);
+
+  function handlePasswordVisibility() {
+    setIsPasswordHidden((isPasswordHidden) => !isPasswordHidden);
+  }
+
+  // RENDER APPROPRIATE SVG ACCORDING TO PASSWORD VISIBILITY
+  const visibleIcon = (
+    <div className="absolute left-2 bottom-[9px] cursor-pointer">
+      {isPasswordHidden ? (
+        <Eye size={24} onClick={handlePasswordVisibility} />
+      ) : (
+        <EyeSlash size={24} onClick={handlePasswordVisibility} />
+      )}
+    </div>
+  );
+
   return (
     <div className="grid h-[88vh] place-items-center">
-      <form className="w-3/4 grid grid-cols-2 gap-10">
-        {/* USERNAME SECTION */}
-        <div className="flex flex-col gap-2">
-          <label
-            htmlFor="customerUsernameId"
-            className="text-lg font-bold"
-          >
-            نام کاربری :
-            <span className="text-gray-500 text-xs">(به لاتین) </span>
-          </label>
-          <input
-            type="text"
-            name="customerUsername"
-            id="customerUsernameId"
-            className="user-info-input"
-          />
-        </div>
-        {/* PASSWORD SECTION */}
-        <div className="flex flex-col gap-2">
-          <label
-            htmlFor="customerPasswordId"
-            className="text-lg font-bold"
-          >
-            رمز عبور:
-          </label>
-          <input
-            type="password"
-            name="customerPassword"
-            id="customerPasswordId"
-            className="user-info-input"
-          />
-        </div>
-        {/* NAME SECTION */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="customerNameId" className="text-lg font-bold">
-            نام:
-          </label>
-          <input
-            type="text"
-            name="customerName"
-            id="customerNameId"
-            className="user-info-input"
-          />
-        </div>
-        {/* LAST NAME SECTION */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="customerLNameId" className="text-lg font-bold">
-            نام خانوادگی:
-          </label>
-          <input
-            type="text"
-            name="customerLName"
-            id="customerLNameId"
-            className="user-info-input"
-          />
-        </div>
-        {/* PHONE NUMBER SECTION */}
-        <div className="flex flex-col gap-2">
-          <label
-            htmlFor="customerPhoneNumId"
-            className="text-lg font-bold"
-          >
-            تلفن همراه:
-            <span className="text-gray-500 text-xs">
-              (جهت هماهنگی ارسال سفارش)
-            </span>
-          </label>
-          <input
-            type="text"
-            inputMode="numeric"
-            name="customerPhoneNum"
-            id="customerPhoneNumId"
-            className="user-info-input"
-          />
-        </div>
-        {/* ADDRESS SECTION */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="customerAddressId" className="text-lg font-bold">
-            آدرس تحویل:
-          </label>
-          <textarea
-            name="customerAddress"
-            id="customerAddressId"
-            cols="10"
-            rows="10"
-            placeholder="به عنوان مثال : شهرك غرب - بلوار خوردين - خيابان توحيد ٣ - پلاك ١ - طبقه ٤"
-            className=" resize-none user-info-input"
-          />
-        </div>
-        <div className="col-span-2 flex justify-end mt-5">
-          <Button classes=" w-1/4">پرداخت</Button>
-        </div>
-      </form>
+      <Formik
+        initialValues={{
+          customerUsername: "",
+          customerPassword: "",
+          customerName: "",
+          customerLName: "",
+          customerPhoneNum: "",
+          customerAddress: "",
+        }}
+        validationSchema={userInfoFormSchema}
+        onSubmit={(values) => {
+          console.log("Form values submitted:", values);
+        }}
+      >
+        <Form className="w-3/4 grid grid-cols-2 gap-10">
+          {/* USERNAME SECTION */}
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="customerUsernameId"
+              className="text-lg font-bold"
+            >
+              نام کاربری :
+              <span className="text-gray-500 text-xs">(به لاتین) </span>
+            </label>
+            <Field
+              type="text"
+              name="customerUsername"
+              id="customerUsernameId"
+              className="user-info-input"
+            />
+            <ErrorMessage
+              name="customerUsername"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
+          {/* PASSWORD SECTION */}
+          <div className="flex flex-col gap-2 relative">
+            <label
+              htmlFor="customerPasswordId"
+              className="text-lg font-bold"
+            >
+              رمز عبور:
+            </label>
+            <Field
+              type={isPasswordHidden ? "password" : "text"}
+              name="customerPassword"
+              id="customerPasswordId"
+              className="user-info-input"
+            />
+            {visibleIcon}
+            <ErrorMessage
+              name="customerPassword"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
+          {/* NAME SECTION */}
+          <div className="flex flex-col gap-2">
+            <label htmlFor="customerNameId" className="text-lg font-bold">
+              نام:
+            </label>
+            <Field
+              type="text"
+              name="customerName"
+              id="customerNameId"
+              className="user-info-input"
+            />
+            <ErrorMessage
+              name="customerName"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
+          {/* LAST NAME SECTION */}
+          <div className="flex flex-col gap-2">
+            <label htmlFor="customerLNameId" className="text-lg font-bold">
+              نام خانوادگی:
+            </label>
+            <Field
+              type="text"
+              name="customerLName"
+              id="customerLNameId"
+              className="user-info-input"
+            />
+            <ErrorMessage
+              name="customerLName"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
+          {/* PHONE NUMBER SECTION */}
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="customerPhoneNumId"
+              className="text-lg font-bold"
+            >
+              تلفن همراه:
+              <span className="text-gray-500 text-xs">
+                (جهت هماهنگی ارسال سفارش)
+              </span>
+            </label>
+            <Field
+              type="text"
+              inputMode="numeric"
+              name="customerPhoneNum"
+              id="customerPhoneNumId"
+              className="user-info-input"
+            />
+            <ErrorMessage
+              name="customerPhoneNum"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
+          {/* ADDRESS SECTION */}
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="customerAddressId"
+              className="text-lg font-bold"
+            >
+              آدرس تحویل:
+            </label>
+            <Field
+              as="textarea"
+              name="customerAddress"
+              id="customerAddressId"
+              cols="10"
+              rows="10"
+              placeholder="به عنوان مثال : شهرك غرب - بلوار خوردين - خيابان توحيد ٣ - پلاك ١ - طبقه ٤"
+              className="resize-none user-info-input"
+            />
+            <ErrorMessage
+              name="customerAddress"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
+          <div className="col-span-2 flex justify-end mt-5">
+            <Button type="submit" classes="w-1/4">
+              پرداخت
+            </Button>
+          </div>
+        </Form>
+      </Formik>
     </div>
   );
 }
--- ghazal-beauty/src/utils/index.js
+++ ghazal-beauty/src/utils/index.js
@@ -2,3 +2,4 @@ export * from "./generateArrow";
 export * from "./validateAdminInput";
 export * from "./validatePriceEdit";
 export * from "./validateAddProduct";
+export * from "./validateUserInfo";
