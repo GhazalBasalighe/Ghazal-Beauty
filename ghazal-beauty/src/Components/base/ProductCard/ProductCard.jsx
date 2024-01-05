d6cfcc4ac3b834f590ffd28581290b31bb246c7f
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2024-01-05 22:23:15 +0330

    refactor: Change ProductCard Ui and make it look bigger

--- ghazal-beauty/src/Components/base/ProductCard/ProductCard.jsx
+++ ghazal-beauty/src/Components/base/ProductCard/ProductCard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import toPersianDigits from "../../../helpers/toPersianDigits";
 export const ProductCard = ({ product }) => {
   return (
     <div dir="rtl">
@@ -9,16 +8,16 @@ export const ProductCard = ({ product }) => {
           <img
             src={`http://localhost:8000/images/products/thumbnails/${product.thumbnail}`}
             alt={product.name}
-            width={100}
+            width={250}
           />
         </Link>
         <div className="flex flex-col gap-4 items-center">
-          <span className="text-xs w-[200px] break-words line-clamp-2">
+          <span className="text-base w-[200px] break-words line-clamp-2">
             {product.name}
           </span>
-          <span className="text-sm self-end vertical-flex gap-1">
-            {toPersianDigits(product.price.toFixed(3))}
-            <span className="text-xs text-gray-500">تومان</span>
+          <span className="text-base self-end vertical-flex gap-1">
+            {product.price.toLocaleString("fa-IR")}
+            <span className="text-sm text-gray-500">تومان</span>
           </span>
         </div>
       </div>
