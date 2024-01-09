355dc5b34571b9c969a23ab486bece04c01b1162
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2024-01-09 13:32:37 +0330

    feat: Install react-query

--- ghazal-beauty/src/main.jsx
+++ ghazal-beauty/src/main.jsx
@@ -6,14 +6,18 @@ import { CheckboxProvider } from "./context/checkboxContext.jsx";
 import { store, persistor } from "./store/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor} />
-      <CheckboxProvider>
-        <App />
-      </CheckboxProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor} />
+        <CheckboxProvider>
+          <App />
+        </CheckboxProvider>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
